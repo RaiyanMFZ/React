9555047d8b5f6b8f371287a189c3b570ec440d86
RaiyanMFZ <sadgidmahfuz@gmail.com>
2025-01-23 14:13:21 +0100

    send

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+import BaseLayout from "../layout/Base";
+
+export default function Contact() {
+    return (
+        <BaseLayout>
+            <h1>Contact page</h1>
+            <p>Test</p>
+        </BaseLayout>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import BaseLayout from "../layout/Base";
+
+export default function Home() {
+    return (
+        <BaseLayout>
+            <h1>Home page</h1>
+            <p>Test</p>
+        </BaseLayout>
+    );
+}
